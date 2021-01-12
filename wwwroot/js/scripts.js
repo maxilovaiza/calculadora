8b4d049acc947f7f5ecaf81b847eb53faf3ca05a
maxilovaiza <68305498+maxilovaiza@users.noreply.github.com>
2021-01-12 19:41:25 -0300

    firts-commit

---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,18 @@
+﻿
+
+
+$("button").click(function () {
+    if (this.id == "=") {
+        $("#txtResultado").val(eval($("#txtResultado").val()))
+    } else {
+        $("#txtResultado").val($("#txtResultado").val() + this.id)
+    }
+    if (this.id == "c") {
+
+        $("#txtResultado").val("")
+
+
+    }
+
+
+});
